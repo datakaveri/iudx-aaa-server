c71a189a324ea474b051f99ad775d8647410a0c2
Gopal Mahajan <gopalmit2601@gmail.com>
2021-09-15 15:21:02 +0530

    Code Refactored.

--- src/main/java/iudx/aaa/server/auditing/util/Constants.java
+++ src/main/java/iudx/aaa/server/auditing/util/Constants.java
@@ -9,19 +9,18 @@ public class Constants {
   public static final String DETAIL = "detail";
   public static final String ERROR_TYPE = "type";
   public static final String TITLE = "title";
-  public static final String RESULTS = "results";
   public static final String STATUS = "status";
 
   /* Database */
+  public static final String ERROR = "Error";
   public static final String QUERY_KEY = "query";
-  public static final String COUNT = "count";
-
-  /* Metering Service Constants*/
+  public static final String DATA_NOT_FOUND="Required Data not Found";
+  /* Auditing Service Constants*/
   public static final String METHOD = "method";
   public static final String USER_ID = "userId";
   public static final String BODY = "body";
   public static final String API = "api";
   public static final String WRITE_QUERY =
-      "INSERT INTO testing_Table (id,body,endpoint,method,time,userid) VALUES ('$1','$2','$3','$4',$5,'$6')";
+      "INSERT INTO testing_table (id,body,endpoint,method,time,userid) VALUES ('$1','$2','$3','$4',$5,'$6')";
   public static final String MESSAGE = "message";
 }
