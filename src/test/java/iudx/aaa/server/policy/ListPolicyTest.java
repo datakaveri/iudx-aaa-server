00ec9d66ee097c4ac66bb677e4b2bc1c3c4d8d49
ThorodanBrom <bryan.robert@datakaveri.org>
2022-03-11 15:48:23 +0530

    Updated list policy tests - Currently not testing APD policies - Added mocking for `catalogueClient`, `apdService` - Updated mocking of `registrationService` (not using `mockRegistrationFactory`)

--- src/test/java/iudx/aaa/server/policy/ListPolicyTest.java
+++ src/test/java/iudx/aaa/server/policy/ListPolicyTest.java
@@ -5,12 +5,19 @@ import static iudx.aaa.server.apiserver.util.Urn.*;
 import static iudx.aaa.server.policy.Constants.*;
 import static iudx.aaa.server.policy.TestRequest.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
 import java.util.Map;
+import java.util.Set;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import org.apache.logging.log4j.LogManager;
@@ -28,143 +35,204 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith({VertxExtension.class, MockitoExtension.class})
 public class ListPolicyTest {
-    private static Logger LOGGER = LogManager.getLogger(iudx.aaa.server.token.TokenServiceTest.class);
+  private static Logger LOGGER = LogManager.getLogger(iudx.aaa.server.policy.ListPolicyTest.class);
 
-    private static Configuration config;
+  private static Configuration config;
 
-// Database Properties
+  // Database Properties
 
-    private static String databaseIP;
-    private static int databasePort;
-    private static String databaseName;
-    private static String databaseSchema;
-    private static String databaseUserName;
-    private static String databasePassword;
-    private static int poolSize;
-    private static PgPool pgclient;
-    private static PoolOptions poolOptions;
-    private static PgConnectOptions connectOptions;
-    private static PolicyService policyService;
-    private static ApdService apdService = Mockito.mock(ApdService.class);
-    private static RegistrationService registrationService;
-    private static CatalogueClient catalogueClient;
-    private static JsonObject authOptions;
-    private static JsonObject catOptions;
+  private static String databaseIP;
+  private static int databasePort;
+  private static String databaseName;
+  private static String databaseSchema;
+  private static String databaseUserName;
+  private static String databasePassword;
+  private static int poolSize;
+  private static PgPool pgclient;
+  private static PoolOptions poolOptions;
+  private static PgConnectOptions connectOptions;
+  private static PolicyService policyService;
+  private static ApdService apdService = Mockito.mock(ApdService.class);
+  private static RegistrationService registrationService;
+  private static CatalogueClient catalogueClient = Mockito.mock(CatalogueClient.class);
+  private static JsonObject authOptions;
+  private static JsonObject catOptions;
 
-    private static Vertx vertxObj;
-    private static MockRegistrationFactory mockRegistrationFactory;
+  private static Vertx vertxObj;
+  private static MockRegistrationFactory mockRegistrationFactory;
 
 
 
-    @BeforeAll
-    @DisplayName("Deploying Verticle")
-    static void startVertx(Vertx vertx, VertxTestContext testContext) {
+  @BeforeAll
+  @DisplayName("Deploying Verticle")
+  static void startVertx(Vertx vertx, VertxTestContext testContext) {
 
-        config = new Configuration();
-        vertxObj = vertx;
-        JsonObject dbConfig = config.configLoader(0, vertx);
+    config = new Configuration();
+    vertxObj = vertx;
+    JsonObject dbConfig = config.configLoader(0, vertx);
 
- //Read the configuration and set the postgres client properties.
+    // Read the configuration and set the postgres client properties.
 
-        LOGGER.debug("Info : Reading config file");
+    LOGGER.debug("Info : Reading config file");
 
-        databaseIP = dbConfig.getString("databaseIP");
-        databasePort = Integer.parseInt(dbConfig.getString("databasePort"));
-        databaseName = dbConfig.getString("databaseName");
-        databaseSchema = dbConfig.getString("databaseSchema");
-        databaseUserName = dbConfig.getString("databaseUserName");
-        databasePassword = dbConfig.getString("databasePassword");
-        poolSize = Integer.parseInt(dbConfig.getString("poolSize"));
-        authOptions = dbConfig.getJsonObject("authOptions");
-        catOptions = dbConfig.getJsonObject("catOptions");
-        
-        /*
-         * Injecting authServerUrl into 'authOptions' from config().'authServerDomain'
-         * TODO - make this uniform
-         */
-        authOptions.put("authServerUrl", dbConfig.getString("authServerDomain"));
+    databaseIP = dbConfig.getString("databaseIP");
+    databasePort = Integer.parseInt(dbConfig.getString("databasePort"));
+    databaseName = dbConfig.getString("databaseName");
+    databaseSchema = dbConfig.getString("databaseSchema");
+    databaseUserName = dbConfig.getString("databaseUserName");
+    databasePassword = dbConfig.getString("databasePassword");
+    poolSize = Integer.parseInt(dbConfig.getString("poolSize"));
+    authOptions = dbConfig.getJsonObject("authOptions");
+    catOptions = dbConfig.getJsonObject("catOptions");
+
+    /*
+     * Injecting authServerUrl into 'authOptions' from config().'authServerDomain' TODO - make this
+     * uniform
+     */
+    authOptions.put("authServerUrl", dbConfig.getString("authServerDomain"));
 
     /* Set Connection Object and schema */
     if (connectOptions == null) {
       Map<String, String> schemaProp = Map.of("search_path", databaseSchema);
 
-      connectOptions = new PgConnectOptions().setPort(databasePort).setHost(databaseIP)
-          .setDatabase(databaseName).setUser(databaseUserName).setPassword(databasePassword)
-          .setProperties(schemaProp);
+      connectOptions =
+          new PgConnectOptions().setPort(databasePort).setHost(databaseIP).setDatabase(databaseName)
+              .setUser(databaseUserName).setPassword(databasePassword).setProperties(schemaProp);
     }
 
- //Pool options
+    // Pool options
 
-        if (poolOptions == null) {
-            poolOptions = new PoolOptions().setMaxSize(poolSize);
-        }
-
- //Create the client pool
-
-        pgclient = PgPool.pool(vertx, connectOptions, poolOptions);
-
-        mockRegistrationFactory = new MockRegistrationFactory();
-        registrationService = mockRegistrationFactory.getInstance();
-        policyService = new PolicyServiceImpl(pgclient, registrationService, apdService,
-            catalogueClient, authOptions, catOptions);
-        testContext.completeNow();
+    if (poolOptions == null) {
+      poolOptions = new PoolOptions().setMaxSize(poolSize);
     }
 
+    // Create the client pool
+
+    pgclient = PgPool.pool(vertx, connectOptions, poolOptions);
+
+    mockRegistrationFactory = new MockRegistrationFactory();
+    registrationService = mockRegistrationFactory.getInstance();
+    policyService = new PolicyServiceImpl(pgclient, registrationService, apdService,
+        catalogueClient, authOptions, catOptions);
+    testContext.completeNow();
+  }
+
+  @AfterAll
+  public static void finish(VertxTestContext testContext) {
+    LOGGER.info("Finishing....");
+    vertxObj.close(testContext.succeeding(response -> testContext.completeNow()));
+  }
+
+  @Test
+  @DisplayName("ListPolicy Success provider")
+  void listPolicySuccessProvider(VertxTestContext testContext) {
+
+    Mockito.doAnswer(i -> {
+      Map<UUID, String> result = new HashMap<UUID, String>();
+      Set<UUID> ids = i.getArgument(0);
+      for (UUID x : ids) {
+        result.put(x, "<cat-id-placeholder>");
+      }
+      return Future.succeededFuture(result);
+    }).when(catalogueClient).getCatIds(any(), any());
+
+    Mockito.doAnswer(i -> {
+      Promise<JsonObject> p = i.getArgument(1);
+      JsonObject result = new JsonObject();
+      List<String> ids = i.getArgument(0);
+      for (String x : ids) {
+        result.put(x, new JsonObject());
+      }
+      p.complete(result);
+      return i.getMock();
+    }).when(apdService).getApdDetails(any(), any());
+
+    Mockito.doAnswer(i -> {
+      Promise<JsonObject> p = i.getArgument(1);
+      JsonObject result = new JsonObject();
+      List<String> ids = i.getArgument(0);
+      for (String x : ids) {
+        result.put(x, new JsonObject());
+      }
+      p.complete(result);
+      return i.getMock();
+    }).when(registrationService).getUserDetails(any(), any());
+
+    policyService.listPolicy(validListPolicyProvider, new JsonObject(),
+        testContext.succeeding(response -> testContext.verify(() -> {
+          assertEquals(URN_SUCCESS.toString(), response.getString(TYPE));
+          testContext.completeNow();
+        })));
+  }
 
 
-    @AfterAll
-    public static void finish(VertxTestContext testContext) {
-        LOGGER.info("Finishing....");
-        vertxObj.close(testContext.succeeding(response -> testContext.completeNow()));
-    }
+  @Test
+  @DisplayName("ListPolicy Success consumer")
+  void listPolicySuccessConsumer(VertxTestContext testContext) {
 
-    @Test
-    @DisplayName("ListPolicy Success provider")
-    void listPolicySuccessProvider(VertxTestContext testContext) {
+    Mockito.doAnswer(i -> {
+      Map<UUID, String> result = new HashMap<UUID, String>();
+      Set<UUID> ids = i.getArgument(0);
+      for (UUID x : ids) {
+        result.put(x, "<cat-id-placeholder>");
+      }
+      return Future.succeededFuture(result);
+    }).when(catalogueClient).getCatIds(any(), any());
 
-        mockRegistrationFactory.setResponse("valid");
-        policyService.listPolicy(validListPolicyProvider, new JsonObject(),
-                testContext.succeeding(response -> testContext.verify(() -> {
-                    assertEquals(URN_SUCCESS.toString(), response.getString(TYPE));
-                    testContext.completeNow();
-                })));
-    }
+    Mockito.doAnswer(i -> {
+      Promise<JsonObject> p = i.getArgument(1);
+      JsonObject result = new JsonObject();
+      List<String> ids = i.getArgument(0);
+      for (String x : ids) {
+        result.put(x, new JsonObject());
+      }
+      p.complete(result);
+      return i.getMock();
+    }).when(apdService).getApdDetails(any(), any());
 
+    Mockito.doAnswer(i -> {
+      Promise<JsonObject> p = i.getArgument(1);
+      JsonObject result = new JsonObject();
+      List<String> ids = i.getArgument(0);
+      for (String x : ids) {
+        result.put(x, new JsonObject());
+      }
+      p.complete(result);
+      return i.getMock();
+    }).when(registrationService).getUserDetails(any(), any());
 
-    @Test
-    @DisplayName("ListPolicy Success consumer")
-    void listPolicySuccessConsumer(VertxTestContext testContext) {
+    policyService.listPolicy(validListPolicyConsumer, new JsonObject(),
+        testContext.succeeding(response -> testContext.verify(() -> {
+          assertEquals(URN_SUCCESS.toString(), response.getString(TYPE));
+          testContext.completeNow();
+        })));
+  }
 
-        mockRegistrationFactory.setResponse("valid");
-        policyService.listPolicy(validListPolicyConsumer, new JsonObject(),
-                testContext.succeeding(response -> testContext.verify(() -> {
-                    assertEquals(URN_SUCCESS.toString(), response.getString(TYPE));
-                    testContext.completeNow();
-                })));
-    }
+  @Test
+  @DisplayName("ListPolicy Success no policy")
+  void listPolicySuccess(VertxTestContext testContext) {
 
-    @Test
-    @DisplayName("ListPolicy Success no policy")
-    void listPolicySuccess(VertxTestContext testContext) {
+    policyService.listPolicy(invalidListPolicy, new JsonObject(),
+        testContext.succeeding(response -> testContext.verify(() -> {
+          assertEquals(URN_SUCCESS.toString(), response.getString(TYPE));
+          testContext.completeNow();
+        })));
+  }
 
-        mockRegistrationFactory.setResponse("valid");
-        policyService.listPolicy(invalidListPolicy, new JsonObject(),
-                testContext.succeeding(response -> testContext.verify(() -> {
-                    assertEquals(URN_SUCCESS.toString(), response.getString(TYPE));
-                    testContext.completeNow();
-                })));
-    }
+  @Test
+  @DisplayName("ListPolicy failure")
+  void listPolicyFailure(VertxTestContext testContext) {
 
-    @Test
-    @DisplayName("ListPolicy failure")
-    void listPolicyFailure(VertxTestContext testContext) {
-
-        mockRegistrationFactory.setResponse("invalid");
-        policyService.listPolicy(validListPolicyProvider, new JsonObject(),
-                testContext.failing(response -> testContext.verify(() -> {
-                    assertEquals(INTERNALERROR, response.getMessage());
-                    testContext.completeNow();
-                })));
-    }
+    Mockito.doAnswer(i -> {
+      Promise<JsonObject> p = i.getArgument(1);
+      p.fail("Failure");
+      return i.getMock();
+    }).when(registrationService).getUserDetails(any(), any());
+    policyService.listPolicy(validListPolicyProvider, new JsonObject(),
+        testContext.failing(response -> testContext.verify(() -> {
+          assertEquals(INTERNALERROR, response.getMessage());
+          testContext.completeNow();
+        })));
+  }
 }
 
