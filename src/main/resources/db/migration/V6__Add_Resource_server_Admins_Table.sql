818670b850c496b51891dcfeac0cf5b9ce985654
Akhil Pankaj <akhil.pankaj@datakaveri.org>
2022-07-28 17:00:21 +0530

    Bug Fix
    Migration file updated Fkey from owner_id to admin_id.

--- src/main/resources/db/migration/V6__Add_Resource_server_Admins_Table.sql
+++ src/main/resources/db/migration/V6__Add_Resource_server_Admins_Table.sql
@@ -12,12 +12,12 @@ ALTER TABLE ONLY resource_server_admins
     ADD CONSTRAINT resource_server_admins_pkey PRIMARY KEY (id);
 
 ALTER TABLE ONLY resource_server_admins
-    ADD CONSTRAINT r_s_admins_owner_id_fkey FOREIGN KEY (owner_id) REFERENCES users(id);
+    ADD CONSTRAINT r_s_admins_owner_id_fkey FOREIGN KEY (admin_id) REFERENCES users(id);
 
 ALTER TABLE ONLY resource_server_admins
     ADD CONSTRAINT r_s_admins_resource_server_id_fkey FOREIGN KEY (resource_server_id) REFERENCES resource_server(id);
 
 ALTER TABLE ONLY resource_server_admins
-    ADD CONSTRAINT unique_server_to_admin UNIQUE (owner_id, resource_server_id);
+    ADD CONSTRAINT unique_server_to_admin UNIQUE (admin_id, resource_server_id);
 
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE resource_server_admins TO ${authUser};
