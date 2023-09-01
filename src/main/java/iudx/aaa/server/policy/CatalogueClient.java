cdd18cdab9ec97159d7467022e2052dcb1ca4fb6
ThorodanBrom <bryan.robert@datakaveri.org>
2023-09-01 21:05:25 +0530

    Bug-Fix: Fix resource group cat item key

--- src/main/java/iudx/aaa/server/policy/CatalogueClient.java
+++ src/main/java/iudx/aaa/server/policy/CatalogueClient.java
@@ -82,7 +82,7 @@ public class CatalogueClient {
   public static final String CAT_RESP_APD_KEY = "apdURL";
   public static final String CAT_RESP_ACCESS_POLICY_KEY = "accessPolicy";
   public static final String CAT_RESP_RES_SERVER_URL_KEY = "resourceServerURL";
-  public static final String CAT_RESP_RES_GROUP_KEY = "resourceServerURL";
+  public static final String CAT_RESP_RES_GROUP_KEY = "resourceGroup";
   public static final String CAT_RESP_PROVIDER_USER_ID_KEY = "providerUserId";
   public static final String CAT_SUCCESS_URN = "urn:dx:cat:Success";
 
@@ -684,8 +684,8 @@ public class CatalogueClient {
         return Future.failedFuture(INTERNALERROR);
       }
 
-      if (!body.containsKey(CAT_RESP_RES_GROUP_KEY)
-          && body.getString(CAT_RESP_RES_GROUP_KEY).matches(UUID_REGEX)) {
+      if (!(body.containsKey(CAT_RESP_RES_GROUP_KEY)
+          && body.getString(CAT_RESP_RES_GROUP_KEY).matches(UUID_REGEX))) {
         LOGGER.error(
             "Failed Catalogue item check : Resource {} does not have `resourceGroup` key or is not UUID",
             itemId.toString());
