662a6a6d559a2229934ad2683aa10b0bf71f14a6
rohithvaidya <rohithvaidya@gmail.com>
2025-04-11 10:34:18 +0530

    added all org handlers

--- src/main/java/iudx/aaa/server/apiserver/OrganizationHandler.java
+++ src/main/java/iudx/aaa/server/apiserver/OrganizationHandler.java
@@ -1,14 +1,13 @@
 package iudx.aaa.server.apiserver;
 
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
-import org.cdpg.dx.aaa.organization.models.OrganizationCreateRequest;
-import org.cdpg.dx.aaa.organization.models.UpdateOrgDTO;
+import org.cdpg.dx.aaa.organization.models.*;
 import org.cdpg.dx.aaa.organization.service.OrganizationService;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.cdpg.dx.aaa.organization.models.Status;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -97,17 +96,125 @@ public class OrganizationHandler {
 
     public void listAllOrganisations(RoutingContext routingContext) {
 
+        organizationService.getOrganizations()
+                .onSuccess(requests -> {
+                    JsonArray jsonArray = new JsonArray();
+                    for (Organization req : requests) {
+                        jsonArray.add(req.toJson());
+                    }
+                    routingContext.response()
+                            .setStatusCode(200)
+                            .putHeader("Content-Type", "application/json")
+                            .end(jsonArray.encode());
+                })
+                .onFailure(err -> {
+                    LOGGER.error("Failed to fetch all org", err);
+                    routingContext.response()
+                            .setStatusCode(500)
+                            .putHeader("Content-Type", "application/json")
+                            .end(new JsonObject().put("error", "Failed to fetch records").encode());
+                });
+
+
     }
 
     public void approveJoinOrganisationRequests(RoutingContext routingContext) {
+
+        JsonObject OrgRequestJson = routingContext.body().asJsonObject();
+        System.out.println(OrgRequestJson);
+        UUID requestId;
+        Status status;
+
+        requestId = UUID.fromString(OrgRequestJson.getString("req_id"));
+        status = Status.fromString(OrgRequestJson.getString("status"));
+
+        organizationService.updateOrganizationJoinRequestStatus(requestId, status)
+                .onSuccess(approved -> {
+                    JsonObject response = new JsonObject()
+                            .put("success", approved)
+                            .put("message", approved ? "Request approved successfully" : "Request could not be approved");
+
+                    routingContext.response()
+                            .setStatusCode(200)
+                            .putHeader("Content-Type", "application/json")
+                            .end(response.encode());
+                })
+                .onFailure(err -> {
+                    LOGGER.error("Failed to approve organization creation request", err);
+                    routingContext.response()
+                            .setStatusCode(500)
+                            .putHeader("Content-Type", "application/json")
+                            .end(new JsonObject()
+                                    .put("success", false)
+                                    .put("error", "Internal Server Error")
+                                    .encode());
+                });
+
 
     }
 
     public void getJoinOrganisationRequests(RoutingContext routingContext) {
 
+        String idParam = String.valueOf(routingContext.queryParam("id"));
+        UUID orgId;
+
+        orgId = UUID.fromString(idParam);
+
+        organizationService.getOrganizationPendingJoinRequests(orgId)
+                .onSuccess(requests -> {
+                    JsonArray jsonArray = new JsonArray();
+                    for (OrganizationJoinRequest req : requests) {
+                        jsonArray.add(req.toJson());
+                    }
+                    routingContext.response()
+                            .setStatusCode(200)
+                            .putHeader("Content-Type", "application/json")
+                            .end(jsonArray.encode());
+                })
+                .onFailure(err -> {
+                    LOGGER.error("Failed to fetch all org join requests", err);
+                    routingContext.response()
+                            .setStatusCode(500)
+                            .putHeader("Content-Type", "application/json")
+                            .end(new JsonObject().put("error", "Failed to fetch records").encode());
+                });
+
+
     }
 
     public void joinOrganisationRequest(RoutingContext routingContext) {
+
+        JsonObject OrgRequestJson = routingContext.body().asJsonObject();
+        OrganizationJoinRequest organizationJoinRequest;
+
+        UUID OrgID;
+        UUID UserID;
+
+        OrgID = UUID.fromString(OrgRequestJson.getString("org_id"));
+        UserID = UUID.fromString(OrgRequestJson.getString("user_id"));
+
+        organizationService.joinOrganizationRequest(OrgID, UserID)
+                .onSuccess(createdRequest -> {
+                    JsonObject response = new JsonObject()
+                            .put("success", true)
+                            .put("payload", createdRequest.toJson());
+
+                    routingContext.response()
+                            .setStatusCode(201)
+                            .putHeader("Content-Type", "application/json")
+                            .end(response.encode());
+                })
+                .onFailure(err -> {
+                    LOGGER.error("Failed to create organization join request", err);
+                    routingContext.response()
+                            .setStatusCode(500)
+                            .putHeader("Content-Type", "application/json")
+                            .end(new JsonObject()
+                                    .put("success", false)
+                                    .put("error", "Internal Server Error")
+                                    .encode());
+                });
+
 
     }
 
@@ -145,6 +252,25 @@ public class OrganizationHandler {
     }
 
     public void getOrganisationRequest(RoutingContext routingContext) {
+
+        organizationService.getAllPendingOrganizationCreateRequests()
+                .onSuccess(requests -> {
+                    JsonArray jsonArray = new JsonArray();
+                    for (OrganizationCreateRequest req : requests) {
+                        jsonArray.add(req.toJson());
+                    }
+                    routingContext.response()
+                            .setStatusCode(200)
+                            .putHeader("Content-Type", "application/json")
+                            .end(jsonArray.encode());
+                })
+                .onFailure(err -> {
+                    LOGGER.error("Failed to fetch all org create requests", err);
+                    routingContext.response()
+                            .setStatusCode(500)
+                            .putHeader("Content-Type", "application/json")
+                            .end(new JsonObject().put("error", "Failed to fetch records").encode());
+                });
 
     }
 
